438fc33d8b918941c84cadc5813259566937afa8
arnfol <arnfol@mail.ru>
2018-04-01 17:41:56 +0300

    sim. add calcCRC function -- not checked

--- sim/tbench/RMAPTargetTop_TB.sv
+++ sim/tbench/RMAPTargetTop_TB.sv
@@ -151,26 +151,65 @@ module RMAPTargetTop_TB ();
 		packet.push_back(transID[ 7:0]);
 		packet.push_back(extAddr);
 		for (int i = 3; i >= 0; i--) begin
-			packet.push_back(addr[i*8 +: 8]);
+			packet.push_back(addr[i]);
 		end
 		for (int i = 2; i >= 0; i--) begin
-			packet.push_back(dataLen[i*8 +: 8]);
+			packet.push_back(dataLen[i]);
 		end
 		packet.push_back(calcCRC(packet)); 
 		for (int i = 3; i >= 0; i--) begin
-			packet.push_back(data[i*8 +: 8]);
+			packet.push_back(data[i]);
 		end
 		packet.push_back(calcCRC(data)); 
 
 		// send packet
-		rxGetsPacket(byte'(packet));
+		rxGetsPacket(packet);
 		
 	endtask : writeRMAP
 
 
 	// calculate CRC for RMAP packets
 	function byte calcCRC(byte data[]);
+		byte crcTable[] = {'h00, 'h91, 'he3, 'h72, 'h07, 'h96, 'he4, 'h75, 
+		                   'h0e, 'h9f, 'hed, 'h7c, 'h09, 'h98, 'hea, 'h7b, 
+		                   'h1c, 'h8d, 'hff, 'h6e, 'h1b, 'h8a, 'hf8, 'h69, 
+		                   'h12, 'h83, 'hf1, 'h60, 'h15, 'h84, 'hf6, 'h67, 
+		                   'h38, 'ha9, 'hdb, 'h4a, 'h3f, 'hae, 'hdc, 'h4d, 
+		                   'h36, 'ha7, 'hd5, 'h44, 'h31, 'ha0, 'hd2, 'h43, 
+		                   'h24, 'hb5, 'hc7, 'h56, 'h23, 'hb2, 'hc0, 'h51, 
+		                   'h2a, 'hbb, 'hc9, 'h58, 'h2d, 'hbc, 'hce, 'h5f, 
+		                   'h70, 'he1, 'h93, 'h02, 'h77, 'he6, 'h94, 'h05, 
+		                   'h7e, 'hef, 'h9d, 'h0c, 'h79, 'he8, 'h9a, 'h0b, 
+		                   'h6c, 'hfd, 'h8f, 'h1e, 'h6b, 'hfa, 'h88, 'h19, 
+		                   'h62, 'hf3, 'h81, 'h10, 'h65, 'hf4, 'h86, 'h17, 
+		                   'h48, 'hd9, 'hab, 'h3a, 'h4f, 'hde, 'hac, 'h3d, 
+		                   'h46, 'hd7, 'ha5, 'h34, 'h41, 'hd0, 'ha2, 'h33, 
+		                   'h54, 'hc5, 'hb7, 'h26, 'h53, 'hc2, 'hb0, 'h21, 
+		                   'h5a, 'hcb, 'hb9, 'h28, 'h5d, 'hcc, 'hbe, 'h2f, 
+		                   'he0, 'h71, 'h03, 'h92, 'he7, 'h76, 'h04, 'h95, 
+		                   'hee, 'h7f, 'h0d, 'h9c, 'he9, 'h78, 'h0a, 'h9b, 
+		                   'hfc, 'h6d, 'h1f, 'h8e, 'hfb, 'h6a, 'h18, 'h89, 
+		                   'hf2, 'h63, 'h11, 'h80, 'hf5, 'h64, 'h16, 'h87, 
+		                   'hd8, 'h49, 'h3b, 'haa, 'hdf, 'h4e, 'h3c, 'had, 
+		                   'hd6, 'h47, 'h35, 'ha4, 'hd1, 'h40, 'h32, 'ha3, 
+		                   'hc4, 'h55, 'h27, 'hb6, 'hc3, 'h52, 'h20, 'hb1, 
+		                   'hca, 'h5b, 'h29, 'hb8, 'hcd, 'h5c, 'h2e, 'hbf, 
+		                   'h90, 'h01, 'h73, 'he2, 'h97, 'h06, 'h74, 'he5, 
+		                   'h9e, 'h0f, 'h7d, 'hec, 'h99, 'h08, 'h7a, 'heb, 
+		                   'h8c, 'h1d, 'h6f, 'hfe, 'h8b, 'h1a, 'h68, 'hf9, 
+		                   'h82, 'h13, 'h61, 'hf0, 'h85, 'h14, 'h66, 'hf7, 
+		                   'ha8, 'h39, 'h4b, 'hda, 'haf, 'h3e, 'h4c, 'hdd, 
+		                   'ha6, 'h37, 'h45, 'hd4, 'ha1, 'h30, 'h42, 'hd3, 
+		                   'hb4, 'h25, 'h57, 'hc6, 'hb3, 'h22, 'h50, 'hc1, 
+		                   'hba, 'h2b, 'h59, 'hc8, 'hbd, 'h2c, 'h5e, 'hcf};
+
+		byte crc = 0;
 		
+		for (int i = 0; i < data.size(); i++) begin
+			crc = crcTable[crc ^ data[i]];
+		end
+
+		return crc
 	endfunction : calcCRC
 
 	// send SpaceWire packet to DUT
