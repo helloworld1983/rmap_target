5dd04bd975a52d5b7f83ef6e17b4da4774eeb0c9
arnfol <arnfol@mail.ru>
2018-12-05 17:32:03 +0300

    src. compiled GenericAPBRegs

--- src/GenericAPBRegs.vhd
+++ src/GenericAPBRegs.vhd
@@ -22,28 +22,13 @@
 -- THE SOFTWARE.
 -------------------------------------------------------------------------------
 
----- to RMAPTargetIPDecoder
---rmapLogicalAddress   : in  std_logic_vetor ( 7 downto 0)        ;
---rmapCommand          : in  std_logic_vetor ( 3 downto 0)        ;
---rmapKey              : in  std_logic_vetor ( 7 downto 0)        ;
---rmapExtendedAddress  : in  std_logic_vetor ( 7 downto 0)        ;
---rmapAddress          : in  std_logic_vetor (31 downto 0)        ;
---rmapDataLength       : in  std_logic_vetor (23 downto 0)        ;
---requestAuthorization : in  std_logic                            ;
---authorizeAck         : out std_logic                            ;
---rejectAck            : out std_logic                            ;
---replyStatus          : out std_logic_vetor ( 7 downto 0)        ;
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 library grlib;
-use grlib.config_types.all;
-use grlib.config.all;
 use grlib.amba.all;
-use grlib.stdlib.all;
-use grlib.devices.all;
 
-entity GenricAPBRegs is 
+entity GenericAPBRegs is 
 	generic (
 		-- apb generics
 		PINDEX   : integer := 0      ;
@@ -52,7 +37,6 @@ entity GenricAPBRegs is
 		PIRQ     : integer := 0      ;
 		-- regs generics
 		REGSNUM  : integer := 1      ;
-		REGSRST  : std_logic_vetor(REGSNUM*32-1 downto 0) := (others => '0');
 		-- leon config generics
 		VENDOR   : integer := 16#ff# ;
 		DEVICE   : integer := 16#ff# ;
@@ -65,37 +49,40 @@ entity GenricAPBRegs is
 		apbi : in  apb_slv_in_type                                ;
 		apbo : out apb_slv_out_type                               ;
 		-- registers
-		regi : in  std_logic_vetor (REGSNUM*32-1 downto 0)        ;
-		rego : out std_logic_vetor (REGSNUM*32-1 downto 0)         
+		regrst : in  std_logic_vector (REGSNUM*32-1 downto 0)     ;
+		regi   : in  std_logic_vector (REGSNUM*32-1 downto 0)     ;
+		rego   : out std_logic_vector (REGSNUM*32-1 downto 0)         
 	);
 end;
 
-architecture behavioral of GenricAPBRegs is
+architecture behavioral of GenericAPBRegs is
 
 	constant PCONFIG : apb_config_type := (
 	  0 => ahb_device_reg ( VENDOR, DEVICE, 0, REVISION, PIRQ),
 	  1 => apb_iobar(PADDR, PMASK));
 
-	signal regs    : std_logic_vetor(REGSNUM*32-1 downto 0);
+	signal regs    : std_logic_vector(REGSNUM*32-1 downto 0);
 	signal regAddr : integer;
+	signal apbAccess : std_logic;
 
 begin 
 
-	regAddr <= to_integer(unsigned(apbi.paddr(31 downto 2), 29));
+	regAddr <= to_integer(unsigned(apbi.paddr(31 downto 2)));
 	rego <= regs;
-
+	apbAccess <= apbi.psel(PINDEX) and apbi.penable;
+	
 	-- write to registers
-	process (reset, clock) begin
-		if (reset = '1') then
-			regs <= REGSRST;
-		elsif (rising_edge(clock)) then
-			if(apbi.psel(PINDEX) and apbi.penable and apbi.pwrite) begin
+	process (rst, clk) begin
+		if (rst = '1') then
+			regs <= regrst;
+		elsif (rising_edge(clk)) then
+			if(apbAccess = '1' and apbi.pwrite = '1') then
 				regs(regAddr*32+31 downto regAddr*32) <= apbi.pwdata;
 			end if;
 		end if;
 	end process;
 
 	-- read from registers
-	apbi.prdata <= regi(regAddr*32+31 downto regAddr*32) when (apbi.psel(PINDEX) and apbi.penable and not apbi.pwrite) else (others => '0');
+	apbo.prdata <= regi(regAddr*32+31 downto regAddr*32) when (apbAccess = '1' and apbi.pwrite = '0') else (others => '0');
 
 end behavioral;
